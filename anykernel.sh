4858568761adccf30917a681bf20380faba991c5
Temnij <kzorin52@gmail.com>
2024-08-26 15:33:18 -0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,9 +4,9 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Sleepy Kernel for Redmi Note 10 Pro & Pro Max by @ itsshashanksp
+kernel.string=Black Kernel for Redmi Note 10 Pro & Pro Max by @ itsshashanksp & Temnij
 do.devicecheck=1
-do.modules=0
+do.modules=1
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
